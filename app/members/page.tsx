d3494127308f2f0509bb41c6eb49e749c23829e5
kaith05 <168144956+kaith05@users.noreply.github.com>
2024-11-30 16:28:14 +0900

    fifth commit

---
+++ app/members/page.tsx
@@ -0,0 +1,24 @@
+const data = {
+    contents: [
+        {
+            id: "1",
+            image: {
+                url:"img-member1.jpg",
+                width: 240,
+                height:240,
+            },
+            name: "デイビッド・チャン"
+            position: "CEO",
+            profile:
+            
+        }
+    ]
+
+
+export default function Page() {
+    return (
+        <div style={{ margin: 20 }}>
+            <h1>メンバーページ</h1>
+        </div>
+    );
+}
