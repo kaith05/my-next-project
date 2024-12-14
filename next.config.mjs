4031db911447d56ec2590bf4cf9a61085a452a29
kaith05 <168144956+kaith05@users.noreply.github.com>
2024-12-14 16:20:19 +0900

    seventh commit

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+import { config } from 'process';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "images.microcms-assets.io",
+            },
+        ],
+    },
+};
+
+export  default nextconfig;
